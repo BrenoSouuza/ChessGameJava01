865c3228912ff86fce437d92d39653d3d35a617f
BrenoSouuza <brenosouuza1999@gmail.com>
2024-08-22 14:18:46 -0300

    Teste

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,7 +6,7 @@ public class Program {
     public static void main(String[] args){
 
         Position pos = new Position(3,5);
-        System.out.println(pos + "11");
+        System.out.println(pos);
     }
 }
 
